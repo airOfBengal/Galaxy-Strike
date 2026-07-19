d15cc303b92c6f01a949e8820479556d14023aed
Md. Atiqul Islam <air.cse.pbn@gmail.com>
2026-07-19 13:07:53 +0600

    player ship movement

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -3,6 +3,12 @@ using UnityEngine.InputSystem;
 
 public class PlayerMovement : MonoBehaviour
 {
+    [Header("Movement Control Parameters")]
+    [SerializeField] Vector2 xMoveRange = new Vector2(10, 10);
+    [SerializeField] Vector2 yMoveRange = new Vector2(5, 5);
+    [SerializeField] float controlSpeed = 50f;
+    Vector2 movementInput;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -12,11 +18,19 @@ public class PlayerMovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        ProcessTranslation();
+    }
+
+    private void ProcessTranslation()
+    {
+        float xOffset = movementInput.x * controlSpeed * Time.deltaTime; // Mathf.Clamp(movementInput.x * controlSpeed * Time.deltaTime, xMoveRange.x, xMoveRange.y);
+        float yOffset = movementInput.y * controlSpeed * Time.deltaTime; // Mathf.Clamp(movementInput.y * controlSpeed * Time.deltaTime, yMoveRange.x, yMoveRange.y);
+
+        transform.localPosition = new Vector3(transform.localPosition.x + xOffset, transform.localPosition.y + yOffset, 0f);
     }
 
     public void OnMove(InputValue value)
     {
-        Debug.Log(value.Get<Vector2>());
+        movementInput = value.Get<Vector2>();
     }
 }
