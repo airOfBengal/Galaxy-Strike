c675fcee14b06a868c856187289e02417232ce52
Md. Atiqul Islam <air.cse.pbn@gmail.com>
2026-08-03 22:22:33 +0600

    add fire direction of laser weapons

--- Assets/Scripts/PlayerWeaponController.cs
+++ Assets/Scripts/PlayerWeaponController.cs
@@ -6,13 +6,15 @@ public class PlayerWeaponController : MonoBehaviour
 {
     [SerializeField] RectTransform crosshair;
     [SerializeField] GameObject[] laserWeapons;
+    [SerializeField] Transform targetPoint;
+    [SerializeField] float targetPointDistance = 100f;
     bool isFiring;
     ParticleSystem[] laserParticles;
 
     private void Start()
     {
         Cursor.visible = false;
-        
+
         laserParticles = new ParticleSystem[laserWeapons.Length];
         for(int i = 0; i < laserWeapons.Length; i++)
         {
@@ -24,6 +26,25 @@ public class PlayerWeaponController : MonoBehaviour
     {
         ProcessFiring();    
         MoveCrosshair();
+        MoveTargetPoint();
+        AimLasers();
+    }
+
+    private void AimLasers()
+    {
+        foreach(GameObject laser in laserWeapons)
+        {
+            Vector3 fireDirection = targetPoint.position - this.transform.position;
+            Quaternion rotationToTarget = Quaternion.LookRotation(fireDirection);
+            laser.transform.rotation = rotationToTarget;
+        }
+    }
+
+    private void MoveTargetPoint()
+    {
+        var mousePosition = Mouse.current.position.ReadValue();
+        var targetPosition = new Vector3(mousePosition.x, mousePosition.y, targetPointDistance);
+        targetPoint.position = Camera.main.ScreenToWorldPoint(targetPosition);
     }
 
     private void MoveCrosshair()
