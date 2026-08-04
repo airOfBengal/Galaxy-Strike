aaddc823bcec4e0e89f52161e1d02aaec12efbc6
Md. Atiqul Islam <air.cse.pbn@gmail.com>
2026-08-04 14:38:51 +0600

    add destroyed vfx

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -2,7 +2,10 @@ using UnityEngine;
 
 public class Enemy : MonoBehaviour
 {
+    [SerializeField]   GameObject destroyedVfx;
+
     private void OnParticleCollision(GameObject other) {
+        Instantiate(destroyedVfx, transform.position, Quaternion.identity);
         Destroy(gameObject);
     }
 }
